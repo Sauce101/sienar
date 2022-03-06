668ecb68564b5379ef336e94061a5ddc8a6c4b13
Sauce101 <michaelsaucedo443@gmail.com>
2022-03-06 13:17:53 -0800

    removed ternary from inside Navbar return to fix text shift

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,6 +26,44 @@ const Navbar = () => {
   const theme = useTheme()
   const isMatch = useMediaQuery(theme.breakpoints.down("md"))
 
+  let tbWide
+  if (isMatch) {
+    tbWide = (
+      <>
+        <Typography
+          sx={{
+            fontSize: "1.5rem",
+            paddingLeft: 2,
+            color: "#fff",
+          }}
+        >
+          SIENAR
+        </Typography>
+        <DrawerComp />
+      </>
+    )
+  } else {
+    tbWide = (
+      <Box sx={{ marginLeft: "auto" }}>
+        {PAGES.map((page, index) => (
+          <Link
+            variant="h6"
+            href={`/${page}`}
+            key={index}
+            underline="hover"
+            sx={{
+              marginLeft: "20px",
+              color: "white",
+              textTransform: "uppercase",
+            }}
+          >
+            {page}
+          </Link>
+        ))}
+      </Box>
+    )
+  }
+
   return (
     <>
       <Box>
@@ -36,40 +74,7 @@ const Navbar = () => {
                 <img src={logo} alt="company logo" height="60px" width="40px" />
               </IconButton>
             </Link>
-            {isMatch ? (
-              <>
-                <Typography
-                  sx={{
-                    fontSize: "1.5rem",
-                    paddingLeft: 2,
-                    color: "#fff",
-                  }}
-                >
-                  SIENAR
-                </Typography>
-                <DrawerComp />
-              </>
-            ) : (
-              <>
-                <Box sx={{ marginLeft: "auto" }}>
-                  {PAGES.map((page, index) => (
-                    <Link
-                      variant="h6"
-                      href={`/${page}`}
-                      key={index}
-                      underline="hover"
-                      sx={{
-                        marginLeft: "20px",
-                        color: "white",
-                        textTransform: "uppercase",
-                      }}
-                    >
-                      {page}
-                    </Link>
-                  ))}
-                </Box>
-              </>
-            )}
+            {tbWide}
           </Toolbar>
         </AppBar>
       </Box>
