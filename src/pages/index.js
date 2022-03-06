e6d2ae8fbdbd1388f53b209fa9fed5010f61f253
Sauce101 <michaelsaucedo443@gmail.com>
2022-03-06 10:06:40 -0800

    Layout bug uploaded to lowercase, changed code to lowercase L

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Seo from "../components/seo"
 import HeroSection from "../components/HeroSection"
 import Ships from "../components/Ships"
