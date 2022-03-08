b3b13210464b35606c8f1dffff9b68c2145c888e
Sauce101 <michaelsaucedo443@gmail.com>
2022-03-07 16:45:21 -0800

    removed mui media query from Hero section to fix loading bug

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,37 +1,30 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-import useMediaQuery from "@mui/material/useMediaQuery"
-import useTheme from "@mui/material/styles/useTheme"
 import { Box } from "@mui/material"
 import Ships from "./Ships"
 
 export default function HeroSection() {
-  const theme = useTheme()
-  const isMatch = useMediaQuery(theme.breakpoints.down("sm"))
-
   return (
     <>
-      {isMatch ? (
-        <Box className="pilotMobile">
-          <StaticImage
-            src="../assets/images/mobile/pilot1920.jpg"
-            alt="tie pilot"
-            layout="fullWidth"
-            style={{ height: "100vh" }}
-          />
-          <Ships />
-        </Box>
-      ) : (
-        <Box className="pilotDesk">
-          <StaticImage
-            src="../assets/images/tp-hero.jpg"
-            alt="tie pilot"
-            layout="fullWidth"
-          />
-          <Ships />
-        </Box>
-      )}
+      <Box className="pilotMobile">
+        <StaticImage
+          src="../assets/images/mobile/pilot1920.jpg"
+          alt="tie pilot"
+          placeholder="blurred"
+          layout="fullWidth"
+          style={{ height: "100vh" }}
+        />
+        <Ships />
+      </Box>
+      <Box className="pilotDesk">
+        <StaticImage
+          src="../assets/images/tp-hero.jpg"
+          alt="tie pilot"
+          layout="fullWidth"
+        />
+        <Ships />
+      </Box>
     </>
   )
 }
